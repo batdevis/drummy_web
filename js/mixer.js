0af19fdde2496041dd35b4e2ce2b82c82d513846
Devis Battisti <devis.battisti@rawfish.com>
2019-08-14 19:02:15 +0200

    mixer refactor

--- js/mixer.js
+++ js/mixer.js
@@ -2,11 +2,14 @@ class Mixer {
   channels = [];
 
   addChannel(channelRef) {
+    console.log('[Mixer] addChannel', channelRef);
     let channel = this.makeChannel(channelRef);
     this.channels.push(channel);
+    return this;
   }
   
   makeChannel(channelRef) {
+    console.log('[Mixer] makeChannel', channelRef);
     var channel = new Tone.Channel().toMaster();
     var player = new Tone.Player({
       url : `./audio/my/A/${channelRef}.[wav|ogg|mp3]`,
@@ -15,20 +18,53 @@ class Mixer {
     player.chain(channel);
 
     //bind the UI
-    document.querySelector(`#${name}`).bind(channel);
-
+    //document.querySelector(`#${channelRef}`).bind(channel);
+    /*
+    let toneChannel = document.getElementById(channelRef);
+    console.log('[Mixer] makeChannel toneChannel', toneChannel);
+    toneChannel.bind(channel);
+    */
     return channel;
   }
 }
 
 function createMixer() {
-  const mixer = new Mixer();
-  let i = 0;
-  for(let i; i < CFG.channel_number.length; i++){
-    let channelRef = "A" + (i + 1);
-    mixer.addChannel(channelRef);
-  }
-  return mixer;
-}
-const mixer = createMixer();
+  console.log('createMixer()');
+  let obj = new Mixer();
+  let tracks = document.getElementById("tracks");
 
+  for(let i=0; i < CFG.channel_number; i++){
+    let channelRef = "A" + (i + 1);
+
+    let toneChannel = document.createElement('tone-channel');
+    toneChannel.label = `CH ${channelRef}`;
+    toneChannel.id = channelRef;
+    console.log('[createMixer()] appendChild', toneChannel);
+    tracks.appendChild(toneChannel);
+
+    obj.addChannel(channelRef);
+  }
+  return obj;
+}
+
+function bindChannels(){
+  for(let i=0; i < CFG.channel_number; i++){
+    let channelRef = "A" + (i + 1);
+    let toneChannel = document.getElementById(channelRef);
+    toneChannel.bind(mixer.channels[i]);
+  }
+}
+
+let mixer;
+
+ready( () => {
+  console.log('ready.');
+  mixer = createMixer();
+  document.querySelector("tone-play-toggle").bind(Tone.Transport);
+
+  async function bc() {
+    await new Promise(resolve => setTimeout(bindChannels, 1000)); // millisec
+  }
+
+  bc();
+});
