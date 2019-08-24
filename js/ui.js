7b7da1bb6acd5920ae008a821570221999fc028f
Devis Battisti <devis.battisti@rawfish.com>
2019-08-24 21:55:25 +0200

    bank

---
+++ js/ui.js
@@ -0,0 +1,214 @@
+const Ui = {
+  printMidiInputs() {
+    if(typeof(Store.midiInputs) === 'undefined'){
+      return;
+    }
+    const midiInputs = document.getElementById("midi_inputs");
+
+    while (midiInputs.hasChildNodes()) {
+      midiInputs.removeChild(midiInputs.firstChild);
+    }
+    Store.midiInputs.inputs.forEach(input => {
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      a.href = '#';
+      a.innerText = input;
+      a.setAttribute('data-device', input);
+      a.addEventListener('click', this.activateMidiInput, false);
+      li.appendChild(a);
+      midiInputs.appendChild(li);
+    });
+  },
+
+  printMidiInput() {
+    const midiInputActive = document.getElementById("midi_input_active");
+    const name = (
+      Store.midiInputs && 
+      Store.midiInputs.active && 
+      Store.midiInputs.active.name
+    ) || '';
+    midiInputActive.innerText = name;
+  },
+
+  activateMidiInput(e) {
+    const device = e.target.getAttribute('data-device');
+    ws.emit('setMidiInput', {device: device});
+  },
+
+  printPedalboard() {
+    const tbody = document.querySelector("#midi_mapping tbody");
+    while (tbody.hasChildNodes()) {
+      tbody.removeChild(tbody.firstChild);
+    }
+
+    Store.cfg.pedalboard_buttons.forEach(button => {
+      let mapping = Store.midiInputs.active.pedalboard.mappings[button.name];
+      console.log('button', button);
+      let ee = []
+      let tr = document.createElement('tr');
+
+      ee[0] = document.createElement('td');
+      ee[0].class = 'midi_mapping_title';
+      ee[0].id = `midi_mapping_${button.name}`;
+      ee[0].innerText = button.title
+
+      ee[1] = document.createElement('td');
+      ee[1].class = 'midi_mapping_description';
+      ee[1].id = `midi_mapping_${button.name}`;
+      ee[1].innerText = button.description;
+       
+      ee[2] = document.createElement('td');
+      ee[2].class = 'midi_mapping_message_type';
+      ee[2].id = `midi_mapping_message_type_${button.name}`;
+      ee[2].innerText = mapping.message_type;
+      
+      ee[3] = document.createElement('td');
+      ee[3].class = 'midi_mapping_channel';
+      ee[3].id = `midi_mapping_channel_${button.name}`;
+      ee[3].innerText = mapping.channel;
+      
+      ee[4] = document.createElement('td');
+      ee[4].class = 'midi_mapping_value';
+      ee[4].id = `midi_mapping_value_${button.name}`;
+      ee[4].innerText = mapping.value;
+
+      ee.forEach(e => tr.appendChild(e));
+      tbody.appendChild(tr);
+    });
+  },
+
+  printFileTree() {
+    if(typeof(Store.fileTree) === 'undefined'){
+      return;
+    }
+    const fileTree = document.getElementById("file_tree");
+
+    while (fileTree.hasChildNodes()) {
+      fileTree.removeChild(fileTree.firstChild);
+    }
+
+    /*
+     *  <ul id="file_tree">
+     *    <li>
+     *      <h4>a</h4>
+     *      <ul>
+     *        <li id="tree_a_a1.wav">a1.wav</li>
+     *        <li id="tree_a_a2.wav>a2.wav</li>
+     *      </ul>
+     *    </li>
+     *
+     *    <li>
+     *      <h4>b</h4>
+     *      <ul>
+     *        <li id="tree_b_b1.wav>b1.wav</li>
+     *        <li id="tree_b_b2.wav>b2.wav</li>
+     *      </ul>
+     *    </li>
+     *  </ul>
+    */
+
+    Store.fileTree.tree.forEach(dir => {
+      let li_0 = document.createElement('li');
+      let h4_0 = document.createElement('h4');
+      h4_0.innerText = dir.name;
+      let ul_0 = document.createElement('ul');
+
+      dir.files.forEach(file => {
+        let pathName = `${dir.name}_${file}`;
+
+        let a = document.createElement('a');
+        a.innerText = file;
+        a.href = '#';
+        a.addEventListener('click', (e) => {
+          e.preventDefault();
+          let channelId = document.querySelector('#file_tree').getAttribute('data-channel-id');
+          let data = {
+            id: channelId,
+            file: file
+          };
+          this.saveChannel(data);
+        });
+        
+        let li_1 = document.createElement('li');
+        li_1.id = `tree_${pathName}`;
+        li_1.appendChild(a);
+        
+        ul_0.appendChild(li_1);
+      })
+
+      li_0.appendChild(h4_0);
+      li_0.appendChild(ul_0);
+
+      fileTree.appendChild(li_0);
+    });
+  },
+
+  saveChannel(data) {
+    console.log('[saveChannel]', data);
+    ws.emit('setChannelFile', data);
+  },
+
+  printChannels() {
+    if(typeof(Store.channels) === 'undefined'){
+      return;
+    }
+    const channelsList = document.getElementById("channels");
+
+    while (channelsList.hasChildNodes()) {
+      channelsList.removeChild(channelsList.firstChild);
+    }
+    Store.channels.forEach(channel => {
+      let li = document.createElement('li');
+      li.id = `channelFile_${channel.id}`;
+      
+      let a = document.createElement('a');
+      a.href = '#';
+      a.innerText = channel.name;
+      a.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.selectChannel(channel);
+      });
+
+      let span = document.createElement('span');
+      span.innerText = `: ${channel.file}`;
+
+      li.appendChild(a);
+      li.appendChild(span);
+
+      channels.appendChild(li);
+    });
+  },
+
+  selectChannel(channel) {
+    const channels = document.getElementById("channels");
+    const children = channels.children;
+    const activeLis = document.querySelector('#channels .active');
+    if (activeLis) {
+      activeLis.classList.remove('active');
+    }
+    if (typeof(channel) !== 'undefined') {
+      const channelLink = document.getElementById(`channelFile_${channel.id}`);
+      channelLink.classList.add('active');
+      this.showFileTree(channel);
+    } else {
+      this.showFileTree();
+    }
+  },
+
+  showFileTree(channel) {
+    console.log('showFileTree', channel);
+    console.log('typeof(channel)', typeof(channel));
+    const fileTreeWrapper = document.getElementById('file_tree_wrapper');
+    const fileTree = document.getElementById('file_tree');
+    if (typeof(channel) !== 'undefined') {
+      const channelName = document.querySelector('#file_tree_wrapper h3 span');
+      const fileTree = document.getElementById('file_tree');
+      fileTree.setAttribute('data-channel-id', channel.id);
+      channelName.innerText = channel.name;
+      fileTreeWrapper.style.display = 'block';
+    } else {
+      fileTreeWrapper.style.display = 'none';
+    }
+  }
+};
+
--- js/ws.js
+++ js/ws.js
@@ -4,23 +4,23 @@ ws.on('midiInputList', function (data) {
   console.log('[ws] midiInputList', data);
   Store.midiInputs = data;
 
-  printMidiInputs();
-  printMidiInput();
-  printPedalboard();
+  Ui.printMidiInputs();
+  Ui.printMidiInput();
+  Ui.printPedalboard();
 });
 
 ws.on('fileTree', function (data) {
   console.log('[ws] fileTree', data);
   Store.fileTree = data;
 
-  printFileTree();
+  Ui.printFileTree();
 });
 
 ws.on('channels', function (data) {
   console.log('[ws] channels', data);
   Store.channels = data.channels;
 
-  printChannels();
+  Ui.printChannels();
 });
 
 ws.on('midiCc', function (data) {
@@ -50,148 +50,3 @@ ws.on('midiCc', function (data) {
   }
 });
 
-function printMidiInputs(){
-  if(typeof(Store.midiInputs) === 'undefined'){
-    return;
-  }
-  const midiInputs = document.getElementById("midi_inputs");
-
-  while (midiInputs.hasChildNodes()) {
-    midiInputs.removeChild(midiInputs.firstChild);
-  }
-  Store.midiInputs.inputs.forEach(input => {
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    a.href = '#';
-    a.innerText = input;
-    a.setAttribute('data-device', input);
-    a.addEventListener('click', activateMidiInput, false);
-    li.appendChild(a);
-    midiInputs.appendChild(li);
-  });
-}
-
-function printMidiInput(){
-  const midiInputActive = document.getElementById("midi_input_active");
-  const name = (
-    Store.midiInputs && 
-    Store.midiInputs.active && 
-    Store.midiInputs.active.name
-  ) || '';
-  midiInputActive.innerText = name;
-}
-
-function activateMidiInput(e){
-  const device = e.target.getAttribute('data-device');
-  ws.emit('setMidiInput', {device: device});
-}
-
-function printPedalboard(){
-  const tbody = document.querySelector("#midi_mapping tbody");
-  while (tbody.hasChildNodes()) {
-    tbody.removeChild(tbody.firstChild);
-  }
-
-  Store.cfg.pedalboard_buttons.forEach(button => {
-    let mapping = Store.midiInputs.active.pedalboard.mappings[button.name];
-    console.log('button', button);
-    let ee = []
-    let tr = document.createElement('tr');
-
-    ee[0] = document.createElement('td');
-    ee[0].class = 'midi_mapping_title';
-    ee[0].id = `midi_mapping_${button.name}`;
-    ee[0].innerText = button.title
-
-    ee[1] = document.createElement('td');
-    ee[1].class = 'midi_mapping_description';
-    ee[1].id = `midi_mapping_${button.name}`;
-    ee[1].innerText = button.description;
-     
-    ee[2] = document.createElement('td');
-    ee[2].class = 'midi_mapping_message_type';
-    ee[2].id = `midi_mapping_message_type_${button.name}`;
-    ee[2].innerText = mapping.message_type;
-    
-    ee[3] = document.createElement('td');
-    ee[3].class = 'midi_mapping_channel';
-    ee[3].id = `midi_mapping_channel_${button.name}`;
-    ee[3].innerText = mapping.channel;
-    
-    ee[4] = document.createElement('td');
-    ee[4].class = 'midi_mapping_value';
-    ee[4].id = `midi_mapping_value_${button.name}`;
-    ee[4].innerText = mapping.value;
-
-    ee.forEach(e => tr.appendChild(e));
-    tbody.appendChild(tr);
-  });
-}
-
-function printFileTree(){
-  if(typeof(Store.fileTree) === 'undefined'){
-    return;
-  }
-  const fileTree = document.getElementById("file_tree");
-
-  while (fileTree.hasChildNodes()) {
-    fileTree.removeChild(fileTree.firstChild);
-  }
-
-  /*
-   *  <ul id="file_tree">
-   *    <li>
-   *      <h4>a</h4>
-   *      <ul>
-   *        <li>a1.wav</li>
-   *        <li>a2.wav</li>
-   *      </ul>
-   *    </li>
-   *
-   *    <li>
-   *      <h4>b</h4>
-   *      <ul>
-   *        <li>b1.wav</li>
-   *        <li>b2.wav</li>
-   *      </ul>
-   *    </li>
-   *  </ul>
-  */
-
-  Store.fileTree.tree.forEach(dir => {
-    let li_0 = document.createElement('li');
-    let h4_0 = document.createElement('h4');
-    h4_0.innerText = dir.name;
-    let ul_0 = document.createElement('ul');
-
-    dir.files.forEach(file => {
-      let pathName = `${dir.name}_${file}`;
-      console.log('pathName', pathName);
-      let li_1 = document.createElement('li');
-      li_1.id = `tree_${pathName}`;
-      li_1.innerText = file;
-      ul_0.appendChild(li_1);
-    })
-
-    li_0.appendChild(h4_0);
-    li_0.appendChild(ul_0);
-
-    fileTree.appendChild(li_0);
-  });
-}
-
-function printChannels(){
-  if(typeof(Store.channels) === 'undefined'){
-    return;
-  }
-  const channels = document.getElementById("channels");
-
-  while (channels.hasChildNodes()) {
-    channels.removeChild(channels.firstChild);
-  }
-  Store.channels.forEach(channel => {
-    let li = document.createElement('li');
-    li.innerText = `${channel.name}: ${channel.file}`;
-    channels.appendChild(li);
-  });
-}
